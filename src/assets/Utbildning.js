5c3252076e9f2d95acacbb5d98ca1bde4abeb5de
youal <youalwol@yahoo.com>
2025-03-07 00:54:44 +0100

    hämtade alla utbildningar från utbildning.js till data.js genom map

---
+++ src/assets/Utbildning.js
@@ -0,0 +1,35 @@
+import halkb from '../assets/halkb.jpg'
+import handled from '../assets/handled.jpg'
+import motorcykel from '../assets/motorcykel.jpg'
+import risk from '../assets/risk.jpg'
+import taxi from '../assets/taxi.jpg'
+import bil from '../assets/bil.avif'
+
+
+const utbildning=[
+    {
+      titel:'KörKort för bil',
+      img:bil
+    },
+    {
+        titel:'KörKort för Mc',
+        img:motorcykel
+    },
+    {
+        titel:'Handledarutbildning',
+        img:handled
+    },
+    {
+        titel:'Halkbana',
+        img:halkb
+    },
+    {
+        titel:'Risk 1',
+        img:risk
+    },
+    {
+        titel:'KörKort för Taxi',
+        img:taxi
+    },
+];
+export default utbildning;
