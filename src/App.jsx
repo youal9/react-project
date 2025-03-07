a47be54d007699c0ac5931a647a79402451840ca
youal <youalwol@yahoo.com>
2025-03-07 01:20:17 +0100

    alla bilder

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@
 
 import './App.css'
 import Data from './Components/Data/Data'
+import Footer from './Components/Footer/Footer'
 import Hero from './Components/Hero/Hero'
+import Kontakt from './Components/Kontakt/Kontakt'
 import Navbar from './Components/Navbar/Navbar'
+import Oss from './Components/Oss/Oss'
+import Pris from './Components/Pris/Pris'
 
 function App() {
   
@@ -12,7 +16,11 @@ function App() {
     <>
     <Navbar/>
       <Hero/>
+      <Oss/>
       <Data/>
+      <Pris/>
+      <Kontakt/>
+      <Footer/>
     </>
   )
 }
